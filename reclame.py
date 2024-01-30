050ad6c1ca6c41bb8f0d75998b2160d96c6d9971
Dion Dijkhuizen <diondijkhuizen1990@hotmail.com>
2024-01-30 20:23:12 +0100

    inclusief opgave 7

--- reclame.py
+++ reclame.py
@@ -5,6 +5,8 @@ def aanbieding_1(smaak,prijs,korting):
 print(aanbieding_1("aardbei",4,0.1))
 
 def inkomsten_totaal1 (inkomsten):
-    return sum(inkomsten)
+    t = sum(inkomsten)
+    btw = t * float(0.09)
+    print(f"Het totaal van alle inkomsten van deze week is {t} euro, waarover {btw} euro betaald dient te worden")
 
 print(inkomsten_totaal1([220, 430, 125, 160, 205, 90, 345]))
