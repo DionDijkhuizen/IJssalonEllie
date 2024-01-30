dd27062644b736b0b3529255dc5fccda2af67887
Dion Dijkhuizen <diondijkhuizen1990@hotmail.com>
2024-01-30 20:08:59 +0100

    inclusief vraag 6

--- reclame.py
+++ reclame.py
@@ -3,3 +3,8 @@ def aanbieding_1(smaak,prijs,korting):
     print(f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {d}0 euro.")
 
 print(aanbieding_1("aardbei",4,0.1))
+
+def inkomsten_totaal1 (inkomsten):
+    return sum(inkomsten)
+
+print(inkomsten_totaal1([220, 430, 125, 160, 205, 90, 345]))
