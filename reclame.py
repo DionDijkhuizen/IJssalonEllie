26efbfa3e900be5760cad8c08ef2ae91e82f4143
Dion Dijkhuizen <diondijkhuizen1990@hotmail.com>
2024-01-31 13:59:51 +0100

    inclusief vraag 11

--- reclame.py
+++ reclame.py
@@ -1,25 +1,31 @@
 def aanbieding_1(smaak,prijs,korting):
     d = prijs - prijs * korting
     print(f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {d}0 euro.")
-print(aanbieding_1("aardbei",4,0.1))
+aanbieding_1("aardbei",4,0.1)
 print()
 
 def inkomsten_totaal1 (inkomsten):
     totaal = sum(inkomsten)
     bedrag = totaal * float(0.09)
     print(f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {bedrag} euro betaald dient te worden")
-print(inkomsten_totaal1([220, 430, 125, 160, 205, 90, 345]))
+inkomsten_totaal1([220, 430, 125, 160, 205, 90, 345])
 print()
 
 def laag_en_hoog (mijn_lijst):
    hoogste = max(mijn_lijst)
    laagste = min(mijn_lijst)
    print([hoogste,laagste])
-print(laag_en_hoog([220, 430, 125, 160, 205, 90, 345]))
+laag_en_hoog([220, 430, 125, 160, 205, 90, 345])
 print()
 
 import statistics
 def gemiddelde(mijn_lijst):
     mean = statistics.mean(mijn_lijst)
     print(f"De gemiddelde inkomsten deze week zijn {mean} euro.")
-print(gemiddelde([220, 430, 125, 160, 205, 90, 345]))
+gemiddelde([220, 430, 125, 160, 205, 90, 345])
+print()
+
+def meervoudig(invoer_lijst):
+    laag_en_hoog(invoer_lijst)
+    
+meervoudig([10, 5, 3, 2, 1, 2, 9])
