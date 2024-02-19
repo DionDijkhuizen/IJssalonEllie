9e2275ca1e03835895b8afa0225a8208845ab1de
DionDijkhuizen <157161346+DionDijkhuizen@users.noreply.github.com>
2024-02-19 18:36:01 +0100

    Add files via upload

--- boekhouding.py
+++ boekhouding.py
@@ -1,23 +1,27 @@
+#imports
 from helper import *
+from presentatie import *
+import csv
 
+#dictionary inkomsten
 inkomsten= {"Aardbeien-ijs-totaal": 1000, 
             "Vanille-ijs-totaal": 2000, 
             "Chocolade-ijs-totaal" : 1500, 
             "Waterijsjes-totaal" : 750 
             }
 
-print()
-
-for a in inkomsten:
-    print(a)
+#Totaal inkomsten
+totaal_inkomsten = som(inkomsten)
 
 print()
-
-Totaal = sum(inkomsten.values())
-print(Totaal)
-
+print(totaal_inkomsten)
 print()
 
-print(som(inkomsten))
+# Presenteer totaal
+presenteer(inkomsten, totaal_inkomsten)
 
-
+#csv
+with open('boekhouding.csv', 'w', newline='') as csvfile:
+    for key, value in inkomsten.items():
+        writer = csv.writer(csvfile,delimiter=';')
+        writer.writerow([key,value])
